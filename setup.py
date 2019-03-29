2f9ed40e7f1814daeca1d57bb67aa7bc2bbe5324
Florian Roth <florian.roth@bsk-consulting.de>
2019-03-29 10:47:09 +0100

    chore: version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="valhallaAPI",
-    version="0.2.0",
+    version="0.2.1",
     author="Nextron",
     author_email="venom14@gmail.com",
     description="Valhalla API Client",
