459311c849ce9696c0449cc3403a7d696ed0022b
Florian Roth <florian.roth@bsk-consulting.de>
2019-04-02 16:28:57 +0200

    v0.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="valhallaAPI",
-    version="0.2.2",
+    version="0.2.4",
     author="Nextron",
     author_email="venom14@gmail.com",
     description="Valhalla API Client",
