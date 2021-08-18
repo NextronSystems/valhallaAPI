fbec841be4cd74d319abc8ce1357b07666b02986
Florian Roth <venom14@gmail.com>
2021-08-18 17:07:08 +0200

    docs: version number in packet setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="valhallaAPI",
-    version="0.5.1",
+    version="0.5.2",
     author="Nextron",
     author_email="florian.roth@nextron-systems.com",
     description="Valhalla API Client",
