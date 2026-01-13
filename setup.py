94e1b222fe77bb3a5c5d685ef355e19ba287d817
Marius Benthin <marius.benthin@nextron-systems.com>
2026-01-13 11:27:16 +0100

    fix: GitHub URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Valhalla API Client",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nextron/valhallaAPI",
+    url="https://github.com/NextronSystems/valhallaAPI",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
