4614b0fd050004e1af1ccc7b6b485a9ef9dc7f3a
Florian Roth <florian.roth@nextron-systems.com>
2026-04-17 12:15:54 +0200

    build: replace setup.py exec version loading

--- setup.py
+++ setup.py
@@ -1,36 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-version = {}
-with open("valhallaAPI/version.py", "r") as fh:
-    exec(fh.read(), version)
-
-setuptools.setup(
-    name="valhallaapi",
-    version=version["__version__"],
-    author="Nextron",
-    author_email="florian.roth@nextron-systems.com",
-    description="Valhalla API Client",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/NextronSystems/valhallaAPI",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'packaging',
-        'requests',
-        'configparser',
-    ],
-    python_requires='~=3.5',
-    entry_points={
-        'console_scripts': [
-            'valhalla-cli = valhallaAPI.valhalla_cli:main',
-        ],
-    },
-)
+setuptools.setup()
